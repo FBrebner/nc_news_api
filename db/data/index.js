a3b49ad49f2dd25dd23ed6f14b5fc4056420082e
northcoders <northcoders@northcoderss-MacBook-Pro.local>
2019-04-05 11:52:56 +0100

    will REALLY fix the app hosting errors

--- db/data/index.js
+++ db/data/index.js
@@ -1,9 +1,11 @@
-let ENV;
-if (process.env.NODE_ENV === "test") {
-  ENV = "test";
-} else {
-  ENV = "development";
-}
+const ENV=process.env.NODE_ENV === 'test' ? 'test' : 'development';
+
+
+// if (process.env.NODE_ENV === "test") {
+//   ENV = "test";
+// } else {
+//   ENV = "development";
+// }
 
 const development = require("./dev-data");
 const test = require("./test-data");
